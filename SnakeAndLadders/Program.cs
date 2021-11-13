1c9c61f1e18d8598db0888bd1ae868a2526b2399
“Enter <anirudhanand984@gmail.com>
2021-11-13 06:59:15 +0530

    [Anirudh] add : created branch UC2-dieRoll

--- SnakeAndLadders/Program.cs
+++ SnakeAndLadders/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace SnakeAndLadders
+namespace SnakeAndLadder
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Snake and Ladder Game");
+            Console.WriteLine("Welcome to the Snake and Ladder game");
+            SnakeAndLadder snk = new SnakeAndLadder();
+            snk.rollDie();
         }
     }
 }
+
--- SnakeAndLadders/SnakeAndLadder.cs
+++ SnakeAndLadders/SnakeAndLadder.cs
@@ -7,7 +7,17 @@ namespace SnakeAndLadder
     class SnakeAndLadder
     {
 
+
         public int playerPosition = 0;
+        Random random = new Random();
+        public int rollDie()
+        {
+
+            int die = random.Next(1, 7);
+            return die;
+        }
+
+
 
     }
 }

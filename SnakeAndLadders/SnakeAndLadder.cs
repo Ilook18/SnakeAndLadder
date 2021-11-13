75bfa4f2b9c2dd40670bdcf209085fc0f011fe46
“Enter <anirudhanand984@gmail.com>
2021-11-13 06:45:38 +0530

    [Anirudh] add : created branch UC1-startTheGame

---
+++ SnakeAndLadders/SnakeAndLadder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+
+        public int playerPosition = 0;
+
+    }
+}
+

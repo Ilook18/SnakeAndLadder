3941a046d3394c06bac70faf42324e11298bc4b7
“Enter <anirudhanand984@gmail.com>
2021-11-13 07:15:07 +0530

    [Anirudh] add : created branch UC4-winningPosition

--- SnakeAndLadders/SnakeAndLadder.cs
+++ SnakeAndLadders/SnakeAndLadder.cs
@@ -7,7 +7,7 @@ namespace SnakeAndLadder
     class SnakeAndLadder
     {
 
-        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100, START = 0;
         public int playerPosition = 0;
         Random random = new Random();
         public int rollDie()
@@ -19,21 +19,29 @@ namespace SnakeAndLadder
 
         public void Play()
         {
-            int rollDie = this.rollDie();
-            int option = random.Next(0, 3);
-            switch (option)
+            while (this.playerPosition < 100)
             {
-                case NO_PLAY: break;
-                case LADDER:
-                    this.playerPosition += rollDie;
-                    break;
-                case SNAKE:
-                    this.playerPosition -= rollDie;
-                    if (this.playerPosition < 0)
-                    {
-                        this.playerPosition = 0;
-                    }
-                    break;
+
+                int rollDie = this.rollDie();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY: break;
+                    case LADDER:
+                        this.playerPosition += rollDie;
+                        if (this.playerPosition > WINNING)
+                        {
+                            this.playerPosition -= rollDie;
+                        }
+                        break;
+                    case SNAKE:
+                        this.playerPosition -= rollDie;
+                        if (this.playerPosition < START)
+                        {
+                            this.playerPosition = START;
+                        }
+                        break;
+                }
             }
         }
 
